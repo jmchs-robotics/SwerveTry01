5b1dc19e01ec8e81f1af81d457d13e45fb3c3fa1
Eric Hall <ewhall18@gmail.com>
2019-07-02 20:53:12 -0600

    New Branch
    Copied subsystem and command for modification to work with SparkMax.

---
+++ src/main/java/frc/robot/commands/SwerveModuleCommandSparkTalon.java
@@ -0,0 +1,26 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.subsystems.SwerveDriveModule;
+import frc.robot.subsystems.SwerveDriveModuleSparkTalon;
+
+public class SwerveModuleCommandSparkTalon extends Command {
+
+	private SwerveDriveModuleSparkTalon mDriveModule;
+
+	public SwerveModuleCommandSparkTalon(SwerveDriveModuleSparkTalon driveModule) {
+		this.mDriveModule = driveModule;
+
+		requires(driveModule);
+	}
+
+	@Override
+	protected void execute() {
+		// TODO: Handle smart swerve drive angling.
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+}
